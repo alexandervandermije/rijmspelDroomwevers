203833327f249e1a263e3bc16f34365aefbcace4
Alexander van der Mije <alexandervandermije@gmail.com>
2017-07-16 17:59:24 +0200

    more Style changes

--- js/main.js
+++ js/main.js
@@ -19,7 +19,24 @@ gameApp.controller('MainController', function MainController($scope, $http) {
 				$scope.totalQuestions++;
 			}
 		})
+
+	// Temporary Local questions
 	
+	// $scope.game.questions = [
+	// 	{question:"In het midden",possibleAnswers:["egaal", "fataal", "ideaal", "centraal"],is:'is',correctAnswer:"centraal", een: false},
+	// 	{question:"Een oor", possibleAnswers:["loog","zoog","boog","oog"], is:'is niet',correctAnswer:"oog",een:true},
+	// 	{question:"Snel", possibleAnswers:["vandaag","maag","traag","vraag"], is:'is niet', correctAnswer:"traag", een: false},
+	// 	{question:"Laag", possibleAnswers:["oog", "hoog", "boog","droog"], is:'is niet', correctAnswer:"hoog", een:false},
+	// 	{question:"Hersens", possibleAnswers:["trein", "plein", "brein","rein"], is:'is', correctAnswer:"brein", een:false},
+	// 	{question:"Groot", possibleAnswers:["klein", "lijn", "pijn","mijn"], is:'is niet', correctAnswer:"klein", een:false},
+	// 	{question:"Een meer", possibleAnswers:["tonijn", "zelfbewustzijn", "welzijn","woestijn"], is:'is niet', correctAnswer:"woestijn", een:true},
+	// 	{question:"Een cirkel", possibleAnswers:["lijn", "plein", "trein","mijn"], is:'is niet', correctAnswer:"lijn", een:false},
+	// 	{question:"Een grapje", possibleAnswers:["gein", "gordijn", "fontein","terrein"], is:'is', correctAnswer:"gein", een:true},
+	// 	{question:"Onweer", possibleAnswers:["weer", "meneer", "verkeer","studeer"], is:'is', correctAnswer:"weer", een:false},
+	// 	{question:"Pijnlijk", possibleAnswers:["zeer", "meer", "neer","ongeveer"], is:'is', correctAnswer:"zeer", een:false},
+	// 	{question:"Noord", possibleAnswers:["zuid", "kruid", "vanuit","spruit"], is:'is niet', correctAnswer:"zuid", een:false}
+	// ];
+
 	// Input Form & = Form validation variables
 	$scope.newQuestion = '';
 	$scope.optionIsEmpty = false;
