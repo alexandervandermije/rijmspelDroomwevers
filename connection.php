29218f0e0d0211dbadacd07a9e333f0bcad0c79c
Alexander van der Mije <alexandervandermije@gmail.com>
2017-09-09 16:27:41 +0200

    added the backgrounds

--- connection.php
+++ connection.php
@@ -1,3 +1,4 @@
 <?php
 	$connection = new mysqli("db.alexandervandermije.nl", "md250932db381367", "0tT7pEVB", "md250932db381367");
+	//$connection = new mysqli("localhost", "root", "", "droomwevers");
 ?>
